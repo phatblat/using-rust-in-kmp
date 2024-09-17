cb11e36e8a3110f32986cd57703f17c40523e56d
Ben Chatelain <ben@octop.ad>
2024-09-16 20:21:06 -0600

    🍏 Call add_numbers from iOS

--- kmp/composeApp/src/iosMain/kotlin/live/ditto/droidcon/rust_in_kmp/Platform.ios.kt
+++ kmp/composeApp/src/iosMain/kotlin/live/ditto/droidcon/rust_in_kmp/Platform.ios.kt
@@ -1,9 +1,17 @@
+@file:OptIn(ExperimentalForeignApi::class)
+
 package live.ditto.droidcon.rust_in_kmp
 
+import kotlinx.cinterop.ExperimentalForeignApi
 import platform.UIKit.UIDevice
+import rust.add_numbers
 
 class IOSPlatform: Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
+
+fun add(a: Int, b: Int) {
+    add_numbers(a, b)
+}
