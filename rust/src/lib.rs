d40b48383af670331a4ca476bafac4b1056fc715
Ben Chatelain <ben@octop.ad>
2024-09-17 18:45:58 -0600

    🔥 Remove example code from rust lib

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,32 +1,9 @@
-use consts::PI;
-use std::f64::consts;
 
 #[unsafe(export_name = "add_numbers")]
 #[unsafe(no_mangle)]
-pub extern "C" fn add(left: i32, right: i32) -> i32 {
+pub extern "C"
+fn add(left: i32, right: i32) -> i32 {
     left + right
-}
-
-enum Shape {
-    Point,
-    Circle(f64),
-    Rect { width: f64, height: f64 },
-}
-
-impl Shape {
-    fn area(&self) -> f64 {
-        match self {
-            Shape::Point => 0.0,
-            Shape::Circle(radius) => PI * radius * radius,
-            Shape::Rect { width, height } => width * height,
-        }
-    }
-}
-
-fn main() {
-    let shape = Shape::Circle(1.2);
-    let area = shape.area();
-    println!("the area is {area}");
 }
 
 #[cfg(test)]
