4d70a4d33832d9c07db63611924a27d2d3954e5c
Ben Chatelain <ben@octop.ad>
2024-09-17 20:11:36 -0600

    🎨 Remove unused imports

--- kmp/shared/src/androidNativeMain/kotlin/live/ditto/droidcon/rust_in_kmp/Platform.androidNative.kt
+++ kmp/shared/src/androidNativeMain/kotlin/live/ditto/droidcon/rust_in_kmp/Platform.androidNative.kt
@@ -1,8 +1,5 @@
 package live.ditto.droidcon.rust_in_kmp
 
-import kotlinx.cinterop.ExperimentalForeignApi
-import rust.add_numbers
-
 class AndroidNativePlatform : Platform {
     override val name: String = "Android version ?"
 }
